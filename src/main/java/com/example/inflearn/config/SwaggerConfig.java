675eb5db1889cd3e8581b6609414b545bde0fea4
User <sda1996@naver.com>
2021-05-26 09:54:57 +0900

    .SwaggerConfig 수정

--- src/main/java/com/example/inflearn/config/SwaggerConfig.java
+++ src/main/java/com/example/inflearn/config/SwaggerConfig.java
@@ -2,6 +2,8 @@ package com.example.inflearn.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
@@ -19,21 +21,20 @@ public class SwaggerConfig {
   private static final Contact DEFAULT_CONCAT = new Contact("임형욱",
     "https://www.naver.com", "sda1996@naver.com");
 
-  private static final ApiInfo DEFAULT_API_INFO = new ApiInfo("Awesome API Title",
-    "My User management REST AAPI service", "1.0", "urn:tos",
+  private static final ApiInfo DEFAULT_API_INFO = new ApiInfo("Inflearn API",
+    "공부용", "1.0", "urn:tos",
     DEFAULT_CONCAT, "Apache 2.0", "http://www.apache.org/licenses/LICENSE-1.0", new ArrayList<>());
 
   private static final Set<String> DEFAULT_PRODUCES_AND_CONSUMES = new HashSet<>(
-    Arrays.asList("application/json","application/xml")); //asList()는 배열형태의 데이터값을 리스트형태로 바꿔줌
+    Arrays.asList("application/json", "application/xml")); //asList()는 배열형태의 데이터값을 리스트형태로 바꿔줌
 
 
   // 관련된정보들을 지정해서 커스터마이징 해준다
   @Bean
-  public Docket api(){
+  public Docket api() {
     return new Docket(DocumentationType.SWAGGER_2)
       .apiInfo(DEFAULT_API_INFO)  //위에서 만든 apiinfo 지정
       .produces(DEFAULT_PRODUCES_AND_CONSUMES) //프로듀스의 대한 지정
       .consumes(DEFAULT_PRODUCES_AND_CONSUMES); //컨슘에 대한 데이터 지정
-
   }
 }
